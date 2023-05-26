61466e6b26ec8ae485bcbbb2e1654bad6f7f9a3b
Осипов Андрей Тимурович <aosipov@bscmsc.ru>
2023-05-26 11:33:16 +0300

    init project

---
+++ src/main/java/com/kuppuch/KuppuchBot/KuppuchBotApplication.java
@@ -0,0 +1,13 @@
+package com.kuppuch.KuppuchBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuppuchBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KuppuchBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kuppuch/KuppuchBot/KuppuchBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuppuch.KuppuchBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KuppuchBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
