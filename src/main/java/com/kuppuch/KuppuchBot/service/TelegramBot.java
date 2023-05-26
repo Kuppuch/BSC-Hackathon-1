7c10be5c4be27325d8d523d4cbe6f2184ee14c9e
Kuppuch <kuppuchr@gmail.com>
2023-05-26 19:43:32 +0300

    check auth

--- src/main/java/com/kuppuch/KuppuchBot/service/TelegramBot.java
+++ src/main/java/com/kuppuch/KuppuchBot/service/TelegramBot.java
@@ -1,14 +1,21 @@
 package com.kuppuch.KuppuchBot.service;
 
 import javax.annotation.PostConstruct;
+import javax.swing.text.html.Option;
 
+import com.kuppuch.KuppuchBot.domain.entity.User;
+import com.kuppuch.KuppuchBot.repository.UserRepository;
 import lombok.extern.log4j.Log4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.HashMap;
+import java.util.Optional;
 
 @Component
 @Log4j
@@ -19,6 +26,11 @@ public class TelegramBot extends TelegramLongPollingBot {
     @Value("${bot.token}")
     private String botToken;
     private UpdateController updateController;
+
+    @Autowired
+    UserRepository userRepository;
+
+    private HashMap<Long, Boolean> emailChecker = new HashMap<>();
 
     public TelegramBot(UpdateController updateController) {
         this.updateController = updateController;
@@ -45,67 +57,117 @@ public class TelegramBot extends TelegramLongPollingBot {
             String messageText = update.getMessage().getText();
             long chatID = update.getMessage().getChatId();
             String messageContent = "";
+            String rejectAuth = "Необходимо авторизоваться\n Ожидается ввод Email";
 
             switch (messageText) {
                 case "/start":
-                    checkEmployee(updateController.checkMail(update));
-                    messageContent = "Привет, " + update.getMessage().getChat().getFirstName() + "\n" +
-                            "Вы состоите в ЦК <b>[Заглушка]</b>\n" +
-                            "Вам могут понадобиться следующие ссылки: \n" +
-                            "<a href='https://meet.jit.si/SK_ISPI'>Заглушка</a>";
+                    if (!updateController.checkMail(update)) {
+                        messageContent = rejectAuth;
+                        emailChecker.put(update.getMessage().getChatId(), true);
+                    } else {
+                        messageContent = "Привет, " + update.getMessage().getChat().getFirstName() + "\n" +
+                                "Вы состоите в ЦК <b>[Заглушка]</b>\n" +
+                                "Вам могут понадобиться следующие ссылки: \n" +
+                                "<a href='https://meet.jit.si/SK_ISPI'>Заглушка</a>";
+                    }
                     buildMessage(messageContent, chatID);
                     break;
                 case "/importamt_people":
-                    checkEmployee(updateController.checkMail(update));
-                    messageContent = "Some text";
+                    if (!updateController.checkMail(update)) {
+                        messageContent = rejectAuth;
+                    } else {
+                        updateController.checkMail(update);
+                        messageContent = "Some text";
+                    }
                     buildMessage(messageContent, chatID);
                     break;
                 case "/chats":
-                    checkEmployee(updateController.checkMail(update));
-                    messageContent = "В этих чатах вас ждут:\n" +
-                            "Чат ЦК " + "Аналитики" + "\n" +
-                            "Чат off-topic " + "Аналитики" + "\n";
+                    if (!updateController.checkMail(update)) {
+                        messageContent = rejectAuth;
+                    } else {
+                        updateController.checkMail(update);
+                        messageContent = "В этих чатах вас ждут:\n" +
+                                "Чат ЦК " + "Аналитики" + "\n" +
+                                "Чат off-topic " + "Аналитики" + "\n";
+                    }
                     buildMessage(messageContent, chatID);
                     break;
                 case "/logins":
-                    checkEmployee(updateController.checkMail(update));
-                    messageContent = "Храните ваши логины в безопасности и не передавайте третим лицам\n" +
-                            "Пользователь - " + "Заглушка" + "\n" +
-                            "login - " + "Заглушка" + "\n" +
-                            "Job Title - " + "Заглушка" + "\n" +
-                            "Manager - " + "Заглушка" + "\n" +
-                            "Локация - "  + "Заглушка" +  "\n" +
-                            "Email - " + "Заглушка" + "\n";
+                    if (!updateController.checkMail(update)) {
+                        messageContent = rejectAuth;
+                    } else {
+                        updateController.checkMail(update);
+                        messageContent = "Храните ваши логины в безопасности и не передавайте третим лицам\n" +
+                                "Пользователь - " + "Заглушка" + "\n" +
+                                "login - " + "Заглушка" + "\n" +
+                                "Job Title - " + "Заглушка" + "\n" +
+                                "Manager - " + "Заглушка" + "\n" +
+                                "Локация - " + "Заглушка" + "\n" +
+                                "Email - " + "Заглушка" + "\n";
+                    }
                     buildMessage(messageContent, chatID);
                     break;
                 case "/notification":
-                    checkEmployee(updateController.checkMail(update));
-                    messageContent = "В ближайшую неделю вас ожидают следующие встречи:\n\n" +
-                            "<b>Понедельник</b>\n" +
-                            "15:00 - 16:00 1-1\n" +
-                            "\n" +
-                            "<b>Вторник</b>\n" +
-                            "15:00 - 16:00 1-2\n";
+                    if (!updateController.checkMail(update)) {
+                        messageContent = rejectAuth;
+                    } else {
+                        updateController.checkMail(update);
+                        messageContent = "В ближайшую неделю вас ожидают следующие встречи:\n\n" +
+                                "<b>Понедельник</b>\n" +
+                                "15:00 - 16:00 1-1\n" +
+                                "\n" +
+                                "<b>Вторник</b>\n" +
+                                "15:00 - 16:00 1-2\n";
+                    }
                     buildMessage(messageContent, chatID);
                     break;
                 case "/library_bsc":
-                    checkEmployee(updateController.checkMail(update));
-                    messageContent = "Предлгаю ознакомиться со следующей литературой\n"+
-                            "<a href='https://habr.com/ru/articles/692524/'>Как аналитику стать разработчиком</a>";
+                    if (!updateController.checkMail(update)) {
+                        messageContent = rejectAuth;
+                    } else {
+                        updateController.checkMail(update);
+                        messageContent = "Предлгаю ознакомиться со следующей литературой\n" +
+                                "<a href='https://habr.com/ru/articles/692524/'>Как аналитику стать разработчиком</a>";
+                    }
                     buildMessage(messageContent, chatID);
                     break;
                 case "/search":
-                    checkEmployee(updateController.checkMail(update));
-                    messageContent = "Тут мы тестируем параметры\n" + update.getMessage().getText();
+                    if (!updateController.checkMail(update)) {
+                        messageContent = rejectAuth;
+                    } else {
+                        updateController.checkMail(update);
+                        messageContent = "Тут мы тестируем параметры\n" + update.getMessage().getText();
+                    }
                     buildMessage(messageContent, chatID);
                     break;
                 case "/feedback":
-                    checkEmployee(updateController.checkMail(update));
-                    messageContent = "Тут тоже мы тестируем параметры\n" + update.getMessage().getText();
+                    if (!updateController.checkMail(update)) {
+                        messageContent = rejectAuth;
+                    } else {
+                        updateController.checkMail(update);
+                        messageContent = "Тут тоже мы тестируем параметры\n" + update.getMessage().getText();
+                    }
                     buildMessage(messageContent, chatID);
                     break;
                 default:
                     messageContent = "Извините, команда не распознана";
+                    boolean ok = emailChecker.get(update.getMessage().getChatId());
+                    if (ok) {
+                        Optional<User> userOptional =  userRepository.findUserByPostAddress(update.getMessage().getText());
+                       if (userOptional.isPresent()) {
+                           User user = userOptional.get();
+                           user.setActive(true);
+                           user.setTelegrammId(update.getMessage().getChatId().toString());
+                           userRepository.save(user);
+                           if(userRepository.findById(user.getId()).get().isActive() &&
+                           userRepository.findById(user.getId()).get().getTelegrammId().length()>0){
+                               messageContent = "Добро пожаловать!";
+                           }else {
+                               messageContent = "Доступ заперщен!";
+                           }
+
+                       }
+                    }
                     buildMessage(messageContent, chatID);
             }
         }
